cb4625017f5854460f827cb02b533fc2e272a95f
Jeremy Kaplan <jdkaplan@metagram.net>
2021-11-11 20:30:53 -0800

    I guess building golangci-lint _is_ buggy after all

--- tools/tools.go
+++ tools/tools.go
@@ -5,7 +5,3 @@
 // the usual Go modules system.  These dependencies are all binaries, so it is
 // impossible to actually build this package.
 package tools
-
-import (
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-)
